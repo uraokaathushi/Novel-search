b5017521288d4b91d9da50bdff1a4258ba4f496f
uraokaathushi <varon.918@gmail.com>
2024-02-27 10:34:01 +0000

    修正7

--- config/application.rb
+++ config/application.rb
@@ -2,7 +2,6 @@ require_relative "boot"
 
 require "rails/all"
 
-require 'listen'
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
