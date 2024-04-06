474b014742d613f2f45d07902bc654bf3ed39d7c
uraokaathushi <varon.918@gmail.com>
2024-04-06 19:14:13 +0000

    投稿日検索

--- app/controllers/public/novels_controller.rb
+++ app/controllers/public/novels_controller.rb
@@ -23,13 +23,27 @@ class Public::NovelsController < ApplicationController
     content = params[:content]
     method = params[:method]
     sort_by = params[:sort_by]
+    start_date = params[:start_date]
+    end_date = params[:end_date]
+    min_star = params[:min_star]
+    max_star = params[:max_star]
+
     @novels = Novel.all
+
     if genre_ids.present?
       @novels = Novel.joins(:genres_selects).where(genres_selects: {genre_id: genre_ids}).distinct
     end
 
     if content.present?
       @novels =  Novel.search_for(@novels, content, method)
+    end
+
+    if start_date.present? && end_date.present?
+      @novels = @novels.where(created_at: start_date..end_date)
+    end
+
+    if min_star.present? && max_star.present?
+      @novels = @novels.where(star: min_star..max_star)
     end
 
     case sort_by
