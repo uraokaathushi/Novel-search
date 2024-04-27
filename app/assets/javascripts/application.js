f1db42d96c12d66fac5289e1214a00f157ef9a36
uraokaathushi <varon.918@gmail.com>
2024-04-27 03:59:50 +0000

    javascript

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,1 +1,19 @@
+// let str1 = 'Welcome ';
+// let str2 = 'to ';
+// let str3 = 'Novel-search!!';
 
+// alert(str1 + str2 + str3);
+
+// メイン部分
+let alertString;
+
+// 作成した関数を呼び出し、変数へ格納
+alertString = addString("Novel-search!!");
+
+//変数の中身をアラートで表示する
+alert(alertString);
+
+function addString(strA){
+  let addStr = "Welcome to " + strA;
+  return addStr;
+}
